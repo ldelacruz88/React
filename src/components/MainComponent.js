ecfd5fe666bc4d7b28f1312fe5628b805a250654
Leandro De La Cruz <tallerestacruz@gmail.com>
2020-11-29 14:08:36 -0500

    Week3React

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -64,6 +65,10 @@ class Main extends Component {
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
