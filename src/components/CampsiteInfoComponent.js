34b1a61ffdfee3d041d746859182bdec80db6120
Leandro De La Cruz <tallerestacruz@gmail.com>
2020-12-08 20:34:21 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -44,8 +44,13 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
   }
 
   render() {
@@ -136,7 +141,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <React.Fragment>
@@ -158,7 +163,7 @@ function RenderComments({ comments }) {
               </div>
             );
           })}
-          <CommentForm />
+          <CommentForm campsiteId={campsiteId} addComment={addComment} />
         </div>
       </React.Fragment>
     );
@@ -184,7 +189,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
