6bb624aa38cf5483b9b9a9d06af22a9254db2654
Leandro De La Cruz <tallerestacruz@gmail.com>
2020-12-10 18:08:32 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -47,7 +47,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.rating,
       values.author,
@@ -143,7 +143,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <React.Fragment>
@@ -165,7 +165,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
               </div>
             );
           })}
-          <CommentForm campsiteId={campsiteId} addComment={addComment} />
+          <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>
       </React.Fragment>
     );
@@ -213,7 +213,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
