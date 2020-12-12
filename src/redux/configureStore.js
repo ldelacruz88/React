95555ff2e3ff1c09b874382afd63bc49812bb581
Leandro De La Cruz <tallerestacruz@gmail.com>
2020-12-12 13:51:16 -0500

    week 5 workshop completed

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -19,7 +19,7 @@ export const ConfigureStore = () => {
         feedbackForm: InitialFeedback,
       }),
     }),
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   );
 
   return store;
